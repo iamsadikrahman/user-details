75d17fb762f306b8b4caccb95dd4287776308cfc
iamsadikrahman <iamsadikrahman@outlook.com>
2023-10-14 21:30:05 +0600

    user details update done

--- src/views/UserDetails.vue
+++ src/views/UserDetails.vue
@@ -1,4 +1,42 @@
 <script setup>
+import { ref, reactive } from 'vue';
+
+
+const user = reactive({
+  name: 'John Doe',
+  email: 'john@example.com',
+  avatar: 'https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80',
+  birthDate: '01/01/2000',
+  description: 'I am a developer and I love to code web applications using Vue.js and Laravel with Tailwind CSS and Vuetify for frontend and Laravel Livewire for backend.',
+})
+
+
+
+const name = ref('')
+const email = ref('')
+const avatar = ref('')
+const birthDate = ref('')
+const description = ref('')
+
+
+// When user click update button then name and email will be updated
+const updateDetails = () => {
+  user.name = name.value
+  user.email = email.value
+  user.avatar = avatar.value
+  user.birthDate = birthDate.value
+  user.description = description.value
+
+  name.value = ''
+  email.value = ''
+  avatar.value = ''
+  birthDate.value = ''
+  description.value = ''
+
+}
+
+
+
 
 </script>
 
@@ -9,55 +47,55 @@
 <!--    Personal Details-->
     <div class="flex flex-col items-center bg-white rounded shadow  space-y-5 p-14">
       <h2 class="text-2xl font-semibold -mt-8">Personal Details</h2>
-      <img class="rounded-full w-56" src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80" alt="Avatar">
-      <h2 class=" text-2xl font-semibold">John Doe</h2>
-      <p class="text-gray-600 text-center">I am a developer and I love to code web applications using Vue.js and Laravel with Tailwind CSS and Vuetify for frontend and Laravel Livewire for backend.</p>
+      <img class="rounded-full w-56" :src="user.avatar" alt="Avatar">
+      <h2 class=" text-2xl font-semibold">{{user.name}}</h2>
+      <p class="text-gray-600 text-center">{{user.description}}</p>
 
     <!-- Birth Date, Email-->
       <div class="flex gap-8  items-center justify-center">
         <div class="flex flex-col items-center shadow rounded p-5 space-y-3">
           <h2 class=" text-2xl font-semibold">Birth Date</h2>
-          <p class=" text-gray-600">01/01/2000</p>
+          <p class=" text-gray-600">{{user.birthDate}}</p>
         </div>
         <div class="flex flex-col items-center  shadow rounded p-5 space-y-3">
           <h2 class=" text-2xl font-semibold">Email</h2>
-          <p class=" text-gray-600">john@example.com</p>
+          <p class=" text-gray-600">{{user.email}}</p>
         </div>
       </div>
     </div>
 <!--    Edit Details-->
     <div class="flex flex-col items-center bg-white rounded shadow space-y-5 p-14">
       <h2 class="text-2xl font-semibold -mt-8">Edit Details</h2>
-      <form action="">
+      <form>
         <div class="flex flex-col space-y-3">
           <div class="flex flex-col space-y-1">
             <label for="name">Name</label>
-            <input type="text" id="name" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" placeholder="Your Name">
+            <input type="text" id="name" v-model="name" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" placeholder="Your Name">
           </div>
-<!--          Edit Avatar-->
+<!--          Edit Avatar Link-->
           <div class="flex flex-col space-y-1">
-            <label for="avatar">Profile Picture</label>
-            <input type="file" id="avatar" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" placeholder="Avatar">
+            <label for="avatar">Profile Picture (*URL)</label>
+            <input type="text" id="avatar" v-model="avatar" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" placeholder="Avatar">
           </div>
 <!--         Edit Description-->
           <div class="flex flex-col space-y-1">
             <label for="desc">Description</label>
-            <textarea id="desc" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" placeholder="Description"></textarea>
+            <textarea id="desc" v-model="description" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" placeholder="Description"></textarea>
           </div>
           <div class="flex flex-col space-y-1">
             <label for="email">Email</label>
-            <input type="email" id="email" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" placeholder="Your Email">
+            <input type="email" id="email" v-model="email" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" placeholder="Your Email">
           </div>
 
 <!--          Edit Date of Birth-->
           <div class="flex flex-col space-y-1">
             <label for="dob">Date of Birth</label>
-            <input type="date" id="dob" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" placeholder="Date of Birth">
+            <input type="date" id="dob" v-model="birthDate" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" placeholder="Date of Birth">
           </div>
 <!--          Edit Button-->
           <div class="flex flex-col space-y-1">
-            <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
-              Edit
+            <button @click="updateDetails" type="button" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+              Update
             </button>
           </div>
         </div>
