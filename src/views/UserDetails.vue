a38e1c297ed43794926edf12406c815af716ac07
iamsadikrahman <iamsadikrahman@outlook.com>
2023-10-14 19:54:37 +0600

    project initial setup done

---
+++ src/views/UserDetails.vue
@@ -0,0 +1,22 @@
+<script setup>
+
+</script>
+
+<template>
+<div class="min-w-full min-h-screen my-14">
+  <h1 class="text-3xl font-bold mb-10 text-center  ">User Details</h1>
+  <section class="grid grid-cols-2 gap-4">
+    <div class="flex flex-col items-center bg-white rounded shadow">
+      <h2 class="text-2xl font-semibold">Personal Details</h2>
+    </div>
+    <div class="flex flex-col items-center bg-white rounded shadow">
+      <h2 class="text-2xl font-semibold">Edit Details</h2>
+    </div>
+  </section>
+</div>
+
+</template>
+
+<style scoped>
+
+</style>
